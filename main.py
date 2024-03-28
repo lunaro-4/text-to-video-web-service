f8a54a6ba5c287f9c5d42939c2cb189f570a36c6
lunaro-4 <pavelike@gmail.com>
2024-03-28 17:47:56 +0300

    Добавил функции рендера и объеденил их в класс

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import os, sys
 
 
 SIZE = (100,100)
+FPS = 20
+LENGH_IN_SECONDS = 1
 
 
+frames_folder = "frames/"
 text = "Supertext"
 
 outfile = "image.png"
@@ -37,21 +40,29 @@ class BaseTextImage(object):
         self.add_text()
 
 
+class RenderTools(object):
+    @staticmethod
+    def render_part(base_img : Image.Image, left_coord: float) -> Image.Image:
+        right_coord = left_coord + SIZE[0]
+        croped = base_img.crop((int(left_coord), int(0), int(right_coord), int(SIZE[1]))) 
+        return croped
 
-def render_part(base_img : Image.Image, left_coord: float) -> Image.Image:
-    right_coord = left_coord + SIZE[0]
-    croped = base_img.crop((int(left_coord), int(0), int(right_coord), int(SIZE[1]))) 
-    return croped
-
+    @staticmethod
+    def part_render_loop(base_img : Image.Image, save_folder: str):
+        if not os.path.isdir(save_folder):
+            os.makedirs(save_folder)
+        total_frames = FPS * LENGH_IN_SECONDS
+        offset = base_img.size[0]/total_frames
+        for frame in range(total_frames):
+            shot = RenderTools.render_part(base_img,offset*frame) 
+            shot.save(f"{frames_folder}{frame}.png","png")
+        pass
 
 
 
 if __name__ == "__main__":
-    base_img = BaseTextImage(text, "tnr.ttf")
-    base_img.save_image(outfile)
-    cords = (0,0,100,100)
-    with Image.open(outfile) as base_img:
-        im_crop=base_img.crop(cords)
-        render_part(base_img, 5).save("func_crop.png","png")
-        im_crop.save("croped.png", "png")
+    base_img = BaseTextImage(text, "tnr.ttf").img
+    base_img.save("base_img.png", "png")
+    RenderTools.render_part(base_img, 5).save("func_crop.png","png")
+    RenderTools.part_render_loop(base_img, frames_folder)
 
