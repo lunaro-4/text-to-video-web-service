2c3fa5775a245bac329131cba8ccb75f97f75e06
lunaro-4 <pavelike@gmail.com>
2024-03-28 16:43:25 +0300

    Создал класс BaseTextImage

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
+#!.venv/bin/python
 from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
 import os, sys
 
 
 SIZE = (100,100)
 
 
-text = ""
-
-
+text = "Supertext"
 
 outfile = "image.png"
 
-im = Image.new("RGB", SIZE)
-im.save(outfile, "png")
+
+
+
+class BaseTextImage(object):
+    def scale_image(self):
+        while self.img.size[0] < self.font.getlength(text):
+            self.img = Image.new("RGB", (self.img.size[0]+1,self.img.size[1]))
+    
+    def add_text(self):
+        draw = ImageDraw.Draw(self.img)
+        draw.text((0,0),text,(255,255,255),self.font)
+
+    def __init__(self, text, font):
+        img = Image.new("RGB", SIZE)
+        self.img = img
+        self.font = ImageFont.truetype(font, 90)
+        self.text = text
+        self.scale_image()
+        self.add_text()
+        self.img.save(outfile, "png")
+
+
+base_img = BaseTextImage(text, "tnr.ttf")
+
+
