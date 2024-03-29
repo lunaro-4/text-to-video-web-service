5a4f507e09b69291f94a3669849e4760da1efa71
lunaro-4 <pavelike@gmail.com>
2024-03-29 12:52:18 +0300

    Исправления в логике

--- text_to_video/logic.py
+++ text_to_video/logic.py
@@ -17,9 +17,8 @@ LENGH_IN_SECONDS = 3
 
 
 frames_folder = "frames/"
-text = "Supertext"
+#text = "Supertext"
 
-outfile = "image.png"
 
 
 
@@ -29,12 +28,12 @@ class BaseTextImage(object):
     def save_image(self,outfile):
         self.img.save(outfile, "png")
     def scale_image(self):
-        while self.img.size[0] < self.font.getlength(text):
+        while self.img.size[0] < self.font.getlength(self.text):
             self.img = Image.new("RGB", (self.img.size[0]+1,self.img.size[1]))
     
     def add_text(self):
         draw = ImageDraw.Draw(self.img)
-        draw.text((0,0),text,(255,255,255),self.font)
+        draw.text((0,0),self.text,(255,255,255),self.font)
 
     def __init__(self, text, font):
         img = Image.new("RGB", SIZE)
--- text_to_video/views.py
+++ text_to_video/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
-
+from text_to_video.logic import main
 
 def main_view(request):
-    var = ""
-    return render(request=request, template_name='base.html')
-# Create your views here.
+    context = {}
+    text = ""
+    try:
+        text = request.GET['text']
+    except Exception:
+        pass
+    main(text, "outpp.mp4")
+    return render(request=request, template_name='base.html', context=context)
